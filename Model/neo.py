a54c396ccccc41343f6e83bf3c24d024d937c076
mbjork1 <mbjork1@users.noreply.github.com>
2024-10-23 11:35:26 +0200

    tilkobling vellykket neo4j

--- Model/neo.py
+++ Model/neo.py
@@ -1,8 +1,8 @@
 from neo4j import GraphDatabase
 
 # Oppsett av databaseforbindelsen
-URI = "neo4j+s://09b7066b.databases.neo4j.io:7687"
-AUTH = ("neo4j", "password!")
+URI = "neo4j+s://09b7066b.databases.neo4j.io"
+AUTH = ("neo4j", "1RWjhvw4XiA1EaB-OSmdOzdKgzP6e3rKvG77avLwHgU")
 
 def __get_connection():
     driver = GraphDatabase.driver(URI, auth=AUTH)

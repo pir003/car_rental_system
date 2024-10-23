aa4bddad951de39078ba35d7b726f2f444462b26
mbjork1 <mbjork1@users.noreply.github.com>
2024-10-23 14:57:01 +0200

    Justering av koden (get connection)

--- Model/neo.py
+++ Model/neo.py
@@ -1,10 +1,10 @@
-from neo4j import GraphDatabase
+from neo4j import GraphDatabase, Driver
 
 # Oppsett av databaseforbindelsen
 URI = "neo4j+s://09b7066b.databases.neo4j.io"
 AUTH = ("neo4j", "1RWjhvw4XiA1EaB-OSmdOzdKgzP6e3rKvG77avLwHgU")
 
-def __get_connection():
+def _get_connection() -> Driver:
     driver = GraphDatabase.driver(URI, auth=AUTH)
     driver.verify_connectivity()
     return driver

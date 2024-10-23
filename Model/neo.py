a16540fa0a79b479d0127edb5c38d30480e9aff2
Anne Hognaland <anne.hognaland@gmail.com>
2024-10-23 15:30:59 +0200

    Oppdatering av uri

--- Model/neo.py
+++ Model/neo.py
@@ -1,7 +1,7 @@
 from neo4j import GraphDatabase, Driver
 
 # Oppsett av databaseforbindelsen
-URI = "neo4j+s://09b7066b.databases.neo4j.io"
+URI = "neo4j+ssc://09b7066b.databases.neo4j.io"
 AUTH = ("neo4j", "1RWjhvw4XiA1EaB-OSmdOzdKgzP6e3rKvG77avLwHgU")
 
 def _get_connection() -> Driver:

7f996d9724aa4023d7797a385c0aaef05d8abe7c
Anne Hognaland <anne.hognaland@gmail.com>
2024-10-27 23:27:09 +0100

    Oppdatering

--- Project/Controller/test.py
+++ Project/Controller/test.py
@@ -1,25 +1,44 @@
 from project import app
 from flask import render_template, request, redirect, url_for
-from project.models.User import findUserByUsername
+from project.model.car import findCarByCarid, createCar, updateCar, deleteCar
+from project.model.customer import findCustomerByname, createCustomer, updateCustomer, deleteCustomer
+from project.model.employee import findEmployeeById, createEmployee, updateEmployee, deleteEmployee
+
 
 #route index
 @app.route('/', methods=["GET", "POST"])
 def index():
+    return render_template("index.html")
 
+@app.route('/add_car', methods=["GET", "POST"])
+def add_car():
     if request.method == "POST":
-        username = request.form["username"]
-        try:
-            user = findUserByUsername(username)
-            data = {
-                "username": user.username,
-                "email": user.email
-            }
-        except err:
-            print (err)
+        make = request.form ["make"]
+        model = request.form ["model"]
+        year = request.form ["year"]
+        location = request.form ["location"]
+        status = request.form ["status"]
+    
+        createCar(make, model, year, location, status)
+        return redirect(url_for("..."))
+    
+    return render_template ("...")
 
-    else:
-        data = {
-            "username": "Not specified",
-            "email": "Not specified"
-        }
-    return render_template('index.html.j2', data = data)
+
+#    if request.method == "POST":
+#        username = request.form["username"]
+#        try:
+#            user = findUserByUsername(username)
+#            data = {
+#                "username": user.username,
+#                "email": user.email
+#            }
+#        except err:
+#            print (err)
+
+#    else:
+#        data = {
+#            "username": "Not specified",
+#            "email": "Not specified"
+#        }
+#    return render_template('index.html.j2', data = data)

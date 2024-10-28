4ec63850346e343857aea4e3300d496e6c0a60b3
mbjork1 <mbjork1@users.noreply.github.com>
2024-10-28 12:31:06 +0100

    oppkobling fra booking til neo (get_connection)

--- Project/Model/booking.py
+++ Project/Model/booking.py
@@ -1,20 +1,21 @@
 from neo4j import GraphDatabase, Driver
+from Model.neo import _get_connection
 from Model.car import findCarByCarid, updateCar
 from Model.customer import findCustomerByname, customerBooking, customerRental
 
 # Oppsett av databaseforbindelsen
-URI = "neo4j+ssc://09b7066b.databases.neo4j.io"
-AUTH = ("neo4j", "1RWjhvw4XiA1EaB-OSmdOzdKgzP6e3rKvG77avLwHgU")
+#URI = "neo4j+ssc://09b7066b.databases.neo4j.io"
+#AUTH = ("neo4j", "1RWjhvw4XiA1EaB-OSmdOzdKgzP6e3rKvG77avLwHgU")
 
-def _get_connection() -> Driver:
-    driver = GraphDatabase.driver(URI, auth=AUTH)
-    driver.verify_connectivity()
-    return driver
+#def _get_connection() -> Driver:
+    #driver = GraphDatabase.driver(URI, auth=AUTH)
+    #driver.verify_connectivity()
+    #return driver
 
 class Booking:
     
-    def __init__(self, driver):
-        selv.driver = driver
+    def __init__(self):
+        selv.driver = _get_connection()
         
     # Metode for bestille bil
     # Sjekke om kunden har en booking inne, sjekke om bilen er tilgjengelig

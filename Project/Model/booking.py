fe24629e0362cf8ff06e78287ad59a4f509a7d2a
mbjork1 <mbjork1@users.noreply.github.com>
2024-10-28 11:49:40 +0100

    Videre jobbing med booking

--- Project/Model/booking.py
+++ Project/Model/booking.py
@@ -1,2 +1,7 @@
 from neo4j import GraphDatabase
+from Model.car import findCarByCarid, updateCar
+from Model.customer import findCustomerByname, customerBooking, customerRental
 
+# Oppsett av databaseforbindelsen
+URI = "neo4j+ssc://09b7066b.databases.neo4j.io"
+AUTH = ("neo4j", "1RWjhvw4XiA1EaB-OSmdOzdKgzP6e3rKvG77avLwHgU")

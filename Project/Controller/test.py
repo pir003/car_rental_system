162d6e14030d5fee86fe5056e41c173fde70d5a7
mbjork1 <mbjork1@users.noreply.github.com>
2024-11-01 16:56:58 +0100

    store endringer - jsonify :)

--- Project/Controller/test.py
+++ Project/Controller/test.py
@@ -1,5 +1,5 @@
 from Project import app
-from flask import render_template, request, redirect, url_for, flash
+from flask import render_template, request, redirect, url_for, flash, jsonify
 from Project.Model.car import Car, find_car_by_carid, find_all_cars, save_car, update_car, delete_car
 from Project.Model.customer import Customer, find_customer_by_name, find_all_customers, save_customer, update_customer, delete_customer, customer_booking, customer_rental
 from Project.Model.employee import Employee, find_employee_by_id, find_all_employees, save_employee, update_employee, delete_employee
@@ -8,173 +8,141 @@ import json
 # Eg har skifta litt varriabelnamn, så lurt å sjekke i "car", "customer" og "employee" om det er ei endring.
 
 
-@app.route('/', methods=["GET", "POST"])
-def index():
-    return render_template("index.html")
+#@app.route('/', methods=["GET", "POST"])
+#def index():
+#    return render_template("index.html")
 
 @app.route('/get_cars', methods=['GET'])
 def query_cars():
     cars = find_all_cars()
-    return render_template("cars.html", cars = cars)
+    return jsonify(cars)
 
 @app.route('/get_cars_by_carid', methods=['POST'])
 def find_car():
-    record = json.loads(request.data)
-    json.dumps(find_car_by_carid(record['car_id']))
-    return render_template("cars.html", record['car_id'])
+    record = request.get_json()
+    car = find_car_by_carid(record['car_id'])
+    return jsonify(car)
 
-@app.route('/save_car', methods=['GET', 'POST'])
+@app.route('/save_car', methods=['POST'])
 def save_car_info():
-    if request.method == 'POST':
-        record = json.loads(request.data)
-        save_car(record['car_id'], record['make'], record['model'], record['year'], record['location'], record['status'])
-        flash("Car saved successfully.", "success")
-        return redirect(url_for("query_cars")) # Same som Delet og Update
-    return render_template("save_or_edit_cars.html")
+    record = request.get_json()
+    save_car(record['car_id'], record['make'], record['model'], record['year'], record['location'], record['status'])
+    return jsonify({"message": "Car saved successfully."})
 
-@app.route('/update_car', methods=['PUT']) 
+@app.route('/update_car/<car_id>', methods=['PUT']) 
 def update_car_info(car_id):
-    record = json.loads(request.data)
-    update_car(record['car_id'], record['make'], record['model'], record['year'], record['location'], record['status'])
-    flash("Car updated successfully.", "success")
-    return render_template("save_or_edit_cars.html") # Same som Sav_cars og Delete
+    record = request.get_json()
+    update_car(car_id, record['make'], record['model'], record['year'], record['location'], record['status'])
+    return jsonify({"message": "Car updated successfully."}) # Same som Sav_cars og Delete
 
 @app.route('/delete_car', methods=['DELETE'])
 def delete_car_info():
-    record = json.loads(request.data)
+    record = request.get_json()
     delete_car(record['car_id'])
-    flash("Car deleted successfully.", "success")
-    return redirect(url_for("query_cars")) # Same som Update og save_car
+    return jsonify({"message": "Car deleted successfully"}) # Same som Update og save_car
 
 @app.route('/get_customers', methods=['GET'])
 def query_cutomers():
     customers = find_all_customers()
-    return render_template("customer.html", customers = customers)
+    return jsonify(customers)
 
 @app.route('/get_customer_by_name', methods=['POST'])
 def find_customer():
-    record = json.loads(request.data)
-    json.dumps(find_customer_by_name(record['name']))
-    return render_template("customer.html", record['name'])
+    record = request.get_json()
+    customer = find_customer_by_name(record['name'])
+    return jsonify(customer)
 
-@app.route('/save_customer', methods=['GET', 'POST'])
+@app.route('/save_customer', methods=['POST'])
 def save_customer_info():
-    if request.method == 'POST':
-        record = json.loads(request.data)
-        save_car(record['name'], record['age'], record['address'])
-        flash("Customer saved successfully.", "success")
-        return redirect(url_for("customer_query")) # Same som Delet og Update
-    return render_template("save_or_edit_customer.html")
+    record = request.get_json()
+    save_customer(record['name'], record['age'], record['address'])
+    return jsonify({"message": "Customer saved successfully"})
 
 @app.route('/update_customer', methods=['PUT']) 
 def update_customer_info():
-    record = json.loads(request.data)
-    update_car(record['name'], record['age'], record['address'])
-    flash("Customer updated successfully.", "success")
-    return render_template("save_or_edit_customers.html") # Same som Sav_cars og Delete
-
+    record = request.get_json()
+    update_customer(record['name'], record['age'], record['address'])
+    return jsonify({"message": "Customer updated successfully"})
+    
 @app.route('/delete_customer', methods=['DELETE'])
 def delete_customer_info():
-    record = json.loads(request.data)
-    delete_car(record['name'])
-    flash("Customer deleted successfully.", "success")
-    return redirect(url_for("query_customers")) # Same som Update og save_car
+    record = request.get_json()
+    delete_customer(record['name'])
+    return jsonify({"message": "Customer deleted successfully"})
+    
 
 @app.route('/get_employee', methods=['GET'])
-def query_employee():
+def find_employee():
     employees = find_all_employees()
-    return render_template("employees.html", employees = employees)
+    return jsonify(employees)
 
 @app.route('/get_employee_by_id', methods=['POST'])
 def find_employee():
-    record = json.loads(request.data)
-    json.dumps(find_employee_by_id(record['employee_id']))
-    return render_template("employess.html", record['employee_id'])
+    record = request.get_json()
+    employee = find_employee_by_id(record['employee_id'])
+    return jsonify(employee)
 
-@app.route('/save_employee', methods=['GET', 'POST'])
+@app.route('/save_employee', methods=['POST'])
 def save_employee_info():
-    if request.method == 'POST':
-        record = json.loads(request.data)
-        save_employee(record['employee_id'], record['name'], record['address'], record['branch'])
-        flash("Employee saved successfully.", "success")
-        return redirect(url_for("query_employees")) # Same som Delet og Update
-    render_template("save_or_edit_employee.htmls")
+    record = request.get_json()
+    save_employee(record['employee_id'], record['name'], record['address'], record['branch'])
+    return jsonify({"message": "Employee saved successfully"})
 
 @app.route('/update_employee', methods=['PUT']) 
-def update_emplyee_info():
-    record = json.loads(request.data)
+def update_employee_info():
+    record = request.get_json()
     update_employee(record['employee_id'], record['name'], record['address'], record['branch'])
-    flash("Employee updated successfully.", "success")
-    return render_template("save_or_edit_employees.html") # Same som Sav_cars og Delete
+    return jsonify({"message": "Employee updated successfully."})
 
-@app.route('/delete_customer', methods=['DELETE'])
+@app.route('/delete_employee', methods=['DELETE'])
 def delete_employee_info():
-    record = json.loads(request.data)
+    record = request.get_json()
     delete_employee(record['employee_id'])
-    flash("Employee deleted successfully.", "success")
-    return redirect(url_for("query_employees")) # Same som Update og save_car
+    return jsonify({"message": "Employee deleted successfully."}) # Same som Update og save_car
 
-@app.route('/order_a_car', methods=['GET','POST'])
+@app.route('/order_a_car', methods=['POST'])
 def order_a_car():
-    if request.method == 'POST':
-        name = request.form['name']
-        car_id = request.form['car_id']
-        booking = Booking()
-        result = booking.order_car(name, car_id)
-
-        if result.get('success'):
-            flash(result['message'], 'success')
-            return redirect (url_for("..."))
-        else:
-            flash(result['error'], 'error')
+    record = request.get_json()
+    booking = Booking()
+    result = booking.order_car(record['name'], record['car_id'])
     
-        return redirect(url_for("query_cars"))
+    if result.get('success'):
+        return jsonify({"message": result['message']})
+    else:
+        return jsonify({"error": result['error']}), 400
 
 @app.route('/cancel_car_order', methods=['POST'])
 def cancel_car():
-    name = request.form['name']
-    car_id = request.form['car_id']
+    record = request.get_json()
     booking = Booking()
-    result = booking.cancel_car_order(name, car_id)
-
-    if result.get('success'):
-        flash(result['message'], 'success')
-        return redirect (url_for("..."))
-    else:
-        flash(result['error'], 'error')
+    result = booking.cancel_car_order(record['name'], record['car_id'])
     
-    return redirect(url_for("query_cars"))
+    if result.get('success'):
+        return jsonify({"message": result['message']})
+    else:
+        return jsonify({"error": result['error']}), 400
 
 @app.route('/rent_car', methods=['POST'])
 def rent_a_car():
-    name = request.form['name']
-    car_id = request.form['car_id']
+    record = request.get_json()
     booking = Booking()
-    result = booking.rent_car(name, car_id)
-
-    if result.get('success'):
-        flash(result['message'], 'success')
-        return redirect (url_for("..."))
-    else:
-        flash(result['error'], 'error')
+    result = booking.rent_car(record['name'], record['car_id'])
     
-    return redirect(url_for("query_cars"))
+    if result.get('success'):
+        return jsonify({"message": result['message']})
+    else:
+        return jsonify({"error": result['error']}), 400  
 
 @app.route('/return_car', methods=['POST'])
 def return_a_car():
-    name = request.form['name']
-    car_id = request.form['car_id']
-    status = request.form['status']
+    record = request.get_json()
     booking = Booking()
-    result = booking.return_car(name, car_id, status)
-
-    if result.get('success'):
-        flash(result['message'], 'success')
-        return redirect (url_for("..."))
-    else:
-        flash(result['error'], 'error')
+    result = booking.return_car(record['name'], record['car_id'], record['status'])
     
-    return redirect(url_for("query_cars"))
+    if result.get('success'):
+        return jsonify({"message": result['message']})
+    else: 
+        return jsonify({"error": result['error']}), 400
 
 #@app.route('/order_car', methods = ["GET", "POST"])
 #def order_car ():
--- server.py
+++ server.py
@@ -6,3 +6,4 @@ app.secret_key = "veldighemmelig"
 
 if __name__ == '__main__':
     app.run(host="localhost", port=5000, debug=True)
+

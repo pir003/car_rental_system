f0e80e06c98ec99407cbe947b8cb89aaf658e873
mbjork1 <mbjork1@users.noreply.github.com>
2024-11-02 17:33:24 +0100

    rette opp i syntaksfeil

--- Project/Model/booking.py
+++ Project/Model/booking.py
@@ -42,8 +42,8 @@ class Booking:
         # Opprette booking-relasjonen mellom kunden og bilen
         # Se om man må endre fra c til c_customer og c til c_car for å forhindre forvirring
         query = (
-            "MATCH (u:customer), (c:Car)"
-            "WHERE u.name = $name AND c.car_id = $car_id"
+            "MATCH (u:customer), (c:Car) "
+            "WHERE u.name = $name AND c.car_id = $car_id "
             "CREATE (u)-[:BOOKED]->(c)"
             )
         with _get_connection().session() as session:
